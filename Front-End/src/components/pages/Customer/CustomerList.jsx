929b3c98d6245d871cb487c0075cc58957f98319
Ngo Kien <ngochingokien@gmail.com>
2024-07-27 23:00:42 +0700

    Add add function to Customer.jsx

--- Front-End/src/components/pages/Customer/CustomerList.jsx
+++ Front-End/src/components/pages/Customer/CustomerList.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { AiFillDelete, AiFillEdit } from 'react-icons/ai';
+import { Button, Stack, IconButton } from '@mui/material';
 import axios from 'axios';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+
 import { Modal, Form, Input, DatePicker, notification } from 'antd';
 import dayjs from 'dayjs';
+import 'dayjs/locale/en';
+import { PlusOutlined } from '@ant-design/icons';
 import 'dayjs/locale/en';
 dayjs.locale('en');
 
 export default function CustomerList() {
   const [rows, setRows] = useState([]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+  const [isAddModalVisible, setIsAddModalVisible] = useState(false);
   const [currentCustomer, setCurrentCustomer] = useState(null);
-  const [form] = Form.useForm();
+  const [editForm] = Form.useForm();
+  const [addForm] = Form.useForm();
 
   useEffect(() => {
     // Fetch data from the API
@@ -21,6 +27,7 @@ export default function CustomerList() {
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
+  // Delete Function
   const handleDelete = useCallback(async (id) => {
     try {
       await axios.delete(`https://66a4b40a5dc27a3c19099545.mockapi.io/Customer/${id}`);
@@ -32,10 +39,11 @@ export default function CustomerList() {
     }
   }, []);
 
+  // Update Function
   const showUpdateModal = (customer) => {
     setCurrentCustomer(customer);
-    setIsModalVisible(true);
-    form.setFieldsValue({
+    setIsEditModalVisible(true);
+    editForm.setFieldsValue({
       name: customer.name,
       phone: customer.phone,
       email: customer.email,
@@ -45,7 +53,7 @@ export default function CustomerList() {
 
   const handleUpdate = async () => {
     try {
-      const values = await form.validateFields();
+      const values = await editForm.validateFields();
       const updatedCustomer = {
         ...currentCustomer,
         ...values,
@@ -57,7 +65,7 @@ export default function CustomerList() {
       setRows((prevRows) =>
         prevRows.map((row) => (row.id === currentCustomer.id ? updatedCustomer : row))
       );
-      setIsModalVisible(false);
+      setIsEditModalVisible(false);
       notification.success({ message: 'Customer updated successfully' });
     } catch (error) {
       console.error('Error updating customer:', error);
@@ -65,52 +73,92 @@ export default function CustomerList() {
     }
   };
 
+  // Add Function
+  const showAddModal = () => {
+    setIsAddModalVisible(true);
+    addForm.resetFields();
+  };
+
+  const handleAdd = async () => {
+    try {
+      const values = await addForm.validateFields();
+      const newCustomer = {
+        ...values,
+        birthday: values.birthday.format('YYYY-MM-DD'),
+        created_at: dayjs().format('DD/MM/YYYY'),
+        updated_at: dayjs().format('DD/MM/YYYY'),
+      };
+
+      const response = await axios.post('https://66a4b40a5dc27a3c19099545.mockapi.io/Customer', newCustomer);
+      setRows((prevRows) => [...prevRows, response.data]);
+      setIsAddModalVisible(false);
+      notification.success({ message: 'Customer added successfully' });
+    } catch (error) {
+      console.error('Error adding customer:', error);
+      notification.error({ message: 'Failed to add customer' });
+    }
+  };
+
+  // Data Table
   const columns = [
     { field: 'name', headerName: 'Name', width: 200 },
     { field: 'phone', headerName: 'Phone', width: 200 },
     { field: 'point', headerName: 'Point', width: 50 },
     { field: 'email', headerName: 'Email', width: 200 },
     { field: 'birthday', headerName: 'Birthday', width: 200 },
-    {
-      field: 'created_at',
-      headerName: 'Created at',
-      width: 200,
-      valueFormatter: (params) => dayjs(params.value).format('DD/MM/YYYY'),
-    },
-    {
-      field: 'updated_at',
-      headerName: 'Updated at',
-      width: 200,
-      valueFormatter: (params) => dayjs(params.value).format('DD/MM/YYYY'),
-    },
+    // {
+    //   field: 'created_at',
+    //   headerName: 'Created at',
+    //   width: 200,
+    //   valueFormatter: (params) => dayjs(params.value).format('DD/MM/YYYY'),
+    // },
+    // {
+    //   field: 'updated_at',
+    //   headerName: 'Updated at',
+    //   width: 200,
+    //   valueFormatter: (params) => dayjs(params.value).format('DD/MM/YYYY'),
+    // },
     {
       field: 'action',
       headerName: 'Action',
       width: 150,
       renderCell: (params) => (
         <>
-         
-          <button
-            onClick={() => showUpdateModal(params.row)}
-            style={{ backgroundColor: 'transparent', border: 'none', cursor: 'pointer', marginRight: '10px' }}
-          >
-            <AiFillEdit style={{ color: 'blue', fontSize: '20px' }} />
-          </button>
+          <Stack direction="row" spacing={1}>
 
-          <button
-            onClick={() => handleDelete(params.id)}
-            style={{ backgroundColor: 'transparent', border: 'none', cursor: 'pointer',  }}
-          >
-            <AiFillDelete style={{ color: 'red', fontSize: '20px' }} />
-          </button>
+            <IconButton
+              color="primary"
+              onClick={() => showUpdateModal(params.row)}
+              style={{ backgroundColor: 'transparent', border: 'none', cursor: 'pointer', marginRight: '10px' }}
+            >
+              <FaEdit />
+            </IconButton>
+            <IconButton
+              color="secondary"
+              onClick={() => handleDelete(params.id)}
+              style={{ backgroundColor: 'transparent', border: 'none', cursor: 'pointer' }}
+            >
+              <FaTrash />
+            </IconButton>
+          </Stack>
+
         </>
       ),
     },
-    
   ];
 
   return (
     <div style={{ height: 600, width: '100%' }}>
+
+      {/* Add button */}
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginBottom: '16px', backgroundColor: 'black', color: 'white' }}
+        onClick={showAddModal}
+      >
+        Add Customer
+      </Button>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -118,13 +166,54 @@ export default function CustomerList() {
         rowsPerPageOptions={[5, 10]}
         disableRowSelectionOnClick
       />
+
+      {/* Update Modal */}
       <Modal
         title="Update Customer"
-        visible={isModalVisible}
+        visible={isEditModalVisible}
         onOk={handleUpdate}
-        onCancel={() => setIsModalVisible(false)}
+        onCancel={() => setIsEditModalVisible(false)}
       >
-        <Form form={form} layout="vertical">
+        <Form form={editForm} layout="vertical">
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true, message: 'Please enter the name' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="phone"
+            label="Phone"
+            rules={[{ required: true, message: 'Please enter the phone number' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[{ required: true, message: 'Please enter the email' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="birthday"
+            label="Birthday"
+            rules={[{ required: true, message: 'Please select the birthday' }]}
+          >
+            <DatePicker format="YYYY-MM-DD" />
+          </Form.Item>
+        </Form>
+      </Modal>
+
+      {/* Add Modal */}
+      <Modal
+        title="Add Customer"
+        visible={isAddModalVisible}
+        onOk={handleAdd}
+        onCancel={() => setIsAddModalVisible(false)}
+      >
+        <Form form={addForm} layout="vertical">
           <Form.Item
             name="name"
             label="Name"
